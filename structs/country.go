06564c87b2498ff384100709c194b9e8c34f7145
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-07-26 19:09:11 +0700

    feat: create country structs

---
+++ structs/country.go
@@ -0,0 +1,13 @@
+package structs
+
+type Country struct {
+	ID uint
+	Name string
+	Iso2 string
+	Iso3 string
+	Capital string
+	Region string
+	Language string
+	CreatedAt interface{}
+	UpdatedAt interface{}
+}
