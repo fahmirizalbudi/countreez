731db5c6557b387eb1b1caa92002736c21311ecd
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-07-27 10:25:37 +0700

    feat: implementing redis cache

--- handlers/country_handler.go
+++ handlers/country_handler.go
@@ -3,12 +3,30 @@ package handlers
 import (
 	"countreez/configs"
 	"countreez/repositories"
+	"countreez/structs"
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CountryIndex(c *gin.Context) {
+	cacheKey := "countries:all"
+
+	cached, err := configs.Redis.Get(configs.Ctx, cacheKey).Result()
+	if err == nil {
+		var countries []structs.Country
+		if err := json.Unmarshal([]byte(cached), &countries); err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status":  "success",
+				"message": "Data retrieved successfully (from redis)",
+				"data":    countries,
+			})
+			return
+		}
+	}
+
 	countries, err := repositories.GetAllCountries(configs.DB)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -17,6 +35,9 @@ func CountryIndex(c *gin.Context) {
 		})
 		return
 	}
+
+	json, _ := json.Marshal(countries)
+	configs.Redis.Set(configs.Ctx, cacheKey, json, 1*time.Minute)
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": "success",
